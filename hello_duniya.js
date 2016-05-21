ebeacfb33733104269a5fd7dcd0f780f79ee7806
sunjeet-khokhar <sunjeet81@gmail.com>
2016-05-22 11:38:18 +1200

    loops and conditions in JS

--- hello_duniya.js
+++ hello_duniya.js
@@ -1,1 +1,21 @@
 alert("namaste...duniya");
+if (5<2){
+var name = "Rama Shankar"
+alert(name);
+} else
+{alert("mental hai kya?")}
+//comments in JS
+/*
+	multi line comment in JS
+*/
+var x = 3
+while(x < 5)
+{
+  alert (x);
+  x = x+1
+  //console.log(x);
+}
+for (var i=1;i<10;i++)
+{
+	console.log("Hello" + i);
+}
